2394293b92cf0034db739e3da9dc1afab3deacad
Raymond Yang <rasymond.yang987654321@gmail.com>
2021-07-29 14:38:35 -0400

    Created a sample workflow with total volume YTD

--- src/main/java/com/cs/rfq/decorator/publishers/dev.java
+++ src/main/java/com/cs/rfq/decorator/publishers/dev.java
@@ -1,9 +1,14 @@
 package com.cs.rfq.decorator.publishers;
 
+import com.cs.rfq.decorator.Rfq;
 import com.cs.rfq.decorator.TradeDataLoader;
+import com.cs.rfq.decorator.extractors.RfqMetadataFieldNames;
+import com.cs.rfq.decorator.extractors.VolumeTradedWithEntityYTDExtractor;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+
+import java.util.Map;
 
 public class dev {
     public static void main(String[] args) {
@@ -18,6 +23,25 @@ public class dev {
         String pth = "src\\test\\resources\\trades\\trades.json";
         TradeDataLoader lder = new TradeDataLoader();
         Dataset<Row> trades = lder.loadTrades(session, pth);
-        System.out.println(trades);
+//        System.out.println(trades);
+
+//        Rfq requests = new Rfq();
+
+        String validRfqJson = "{" +
+                "'id': '123ABC', " +
+                "'traderId': 3351266293154445953, " +
+                "'entityId': 5561279226039690843, " +
+                "'instrumentId': 'AT0000383864', " +
+                "'qty': 250000, " +
+                "'price': 1.58, " +
+                "'side': 'B' " +
+                "}";
+
+        Rfq requests = Rfq.fromJson(validRfqJson);
+
+        VolumeTradedWithEntityYTDExtractor volExtractor = new VolumeTradedWithEntityYTDExtractor();
+        Map<RfqMetadataFieldNames, Object>  volMap = volExtractor.extractMetaData(requests, session, trades);
+        System.out.println(requests.getIsin() + ": " + volMap);
+
     }
 }
