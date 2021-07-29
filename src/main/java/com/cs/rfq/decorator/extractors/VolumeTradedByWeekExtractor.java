469d4bb10fde24edd4f3a6adc02b6b337f9516b8
Associate <ceciliazeng98@gmail.com>
2021-07-29 18:06:37 -0400

    use the TradeDataLoader to load the trade data archives

---
+++ src/main/java/com/cs/rfq/decorator/extractors/VolumeTradedByWeekExtractor.java
@@ -0,0 +1,49 @@
+package com.cs.rfq.decorator.extractors;
+
+import com.cs.rfq.decorator.Rfq;
+import org.apache.commons.lang.time.DateUtils;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.joda.time.DateTime;
+
+import java.sql.SQLOutput;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class VolumeTradedByWeekExtractor implements RfqMetadataExtractor{
+
+
+    private String since;
+    Date yourDate = DateUtils.addDays(new Date(), -7);
+    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    String strDate = dateFormat.format(yourDate);
+
+    public VolumeTradedByWeekExtractor() {
+        this.since = strDate;
+    }
+    @Override
+    public Map<RfqMetadataFieldNames, Object> extractMetaData(Rfq rfq, SparkSession session, Dataset<Row> trades) {
+        String query = String.format("SELECT SUM(LastQty) from trade where EntityId='%s' AND SecurityId='%s' AND TradeDate >= '%s'",
+                rfq.getEntityId(),
+                rfq.getIsin(),
+                since);
+
+        trades.createOrReplaceTempView("trade");
+        Dataset<Row> sqlQueryResults = session.sql(query);
+
+        Object volume = sqlQueryResults.first().get(0);
+        if (volume == null) {
+            volume = 0L;
+        }
+        Map<RfqMetadataFieldNames, Object> results = new HashMap<>();
+        results.put(RfqMetadataFieldNames.tradesWithEntityPastWeek, volume);
+        return results;
+    }
+    protected void setSince(String since) {
+        this.since = since;
+    }
+}
